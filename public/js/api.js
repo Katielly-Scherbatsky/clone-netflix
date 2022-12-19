5a362376329adcf51eb241b58f772604dbbcf34d
Katielly <katiellyscherbatsky@gmail.com>
2022-12-19 16:16:41 -0400

    initial commit

---
+++ public/js/api.js
@@ -0,0 +1,6 @@
+let api_key = "97da7f4b1531e6407d183a05c9d4f0fe";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
